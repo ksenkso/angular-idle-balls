b8a32e5449409995cb7697ed222cabc1b1a94508
ksenkso <yazunya@gmail.com>
2020-08-09 04:22:33 +0300

    shop component

---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,28 @@
+import {Component} from '@angular/core';
+import {BallsService} from '../balls.service';
+import BallType, {BALL_TYPE} from '../BallType';
+import {PlaygroundService} from '../playground.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.css']
+})
+export class ShopComponent {
+  public ballTypes: BallType[];
+
+  constructor(
+    public playgroundService: PlaygroundService,
+    private ballsService: BallsService
+  ) {
+    this.ballTypes = ballsService.ballTypes;
+  }
+
+  buy(type: BALL_TYPE): void {
+    this.ballsService.buy(type);
+  }
+
+  upgrade(type: BALL_TYPE): void {
+    this.ballsService.upgrade(type);
+  }
+}
