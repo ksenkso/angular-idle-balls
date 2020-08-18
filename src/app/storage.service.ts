ec1b5efbc62e24269e4e880bc7129dfe314f01f2
ksenkso <yazunya@gmail.com>
2020-08-18 19:48:01 +0300

    use app version to set localStorage keys

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -1,10 +1,13 @@
 import {Injectable} from '@angular/core';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export abstract class StorageService<T> {
-  protected constructor(protected key: string) {
+  protected key: string;
+  protected constructor(key: string) {
+    this.key = `${key}/${environment.version}`;
     this.store = this.store.bind(this);
     this.load = this.load.bind(this);
     this.bindStore();
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import {version} from '../../package.json';
 export const environment = {
-  production: true
+  production: true,
+  version,
 };
